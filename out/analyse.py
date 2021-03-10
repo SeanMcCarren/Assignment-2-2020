8629203cc440584dcc8de37539c1b416fc5cc2f8
SeanMcCarren <mccarrensean@gmail.com>
2021-03-10 13:55:01 +0100

    Multiprocessing & Saving intermediate results

--- out/analyse.py
+++ out/analyse.py
@@ -9,6 +9,8 @@ import numpy as np
 import subprocess
 import json
 import bisect
+import multiprocessing as mp
+import time
 
 
 releases = []
@@ -17,6 +19,41 @@ DIR = "../usr/jquery-data/"
 
 def release(path):
     without_dir = path[len(DIR):path.index("/", len(DIR))]
+
+class Results:
+    def __init__(self):
+        self.DONE = {}
+        try:
+            with open('results.txt', mode='r') as f:
+                lines = f.readlines()
+            for line in lines:
+                spl = line.split(",")
+                key = self.__key__(spl[0], spl[1])
+                self.DONE[key] = float(spl[2])
+        except FileNotFoundError:
+            pass
+    
+    def __key__(self, v1, v2):
+        if v1 < v2 :
+            return v1 + "," + v2
+        else :
+            return v2 + "," + v1
+
+    def add(self, ver1, ver2, metric):
+        key = self.__key__(ver1, ver2)
+        self.DONE[key] = metric
+    
+    def has(self, ver1, ver2):
+        key = self.__key__(ver1, ver2)
+        result = self.DONE.get(key, None)
+        return result != None
+
+    def save(self):
+        with open('results.txt', mode='w+') as f:
+            for key in self.DONE:
+                metric = self.DONE[key]
+                f.write(key + ',' + str(metric) + '\n')
+
 
 class Segments:
     segments = []
@@ -68,7 +105,18 @@ def CalculateCloneSegments(json_object):
 
     return sum(lines[path].lines() for path in lines)
 
+def RunJsInspect(result_queue, command, i_1, i_2):
+    print("\n\n RUNNING %d AND %d" % (i_1, i_2))
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    string = result.stdout.decode('utf-8')
+    parsed = json.loads(string)
+    print("\n\n DONE %d AND %d" % (i_1, i_2))
+    Cij = CalculateCloneSegments(parsed)
+    result_queue.put( (i_1, i_2, Cij ))
+
+
 if __name__ == "__main__":
+    START_TIME = time.time()
     assert len(sys.argv) > 1
     t_parameter = str(sys.argv[1])
 
@@ -79,11 +127,12 @@ if __name__ == "__main__":
     
     print("\nFOUND %d RELEASES" % len(releases))
     
+    #releases = releases[:6]
     # tmp = releases.copy()#[:3]
     # releases = [tmp[0]]
     # releases.append(tmp[14])
 
-    RESULT = np.empty((len(releases),len(releases)), dtype=float)
+    RESULTS = Results()
 
     print("\nCOUNTING LINES OF JAVASCRIPT PER RELEASE\n")
 
@@ -100,17 +149,61 @@ if __name__ == "__main__":
 
     print("\nCOMPUTING PAIRWISE SIMILARITY\n")
 
+    QUEUE = []
+    RESULT_QUEUE = mp.Queue()
     for i_1, release_1 in enumerate(releases):
         for i_2, release_2 in enumerate(releases[:i_1]):
-            print("\n\n RUNNING %d AND %d" % (i_1, i_2))
+            if RESULTS.has(release_1['tag'], release_2['tag']):
+                continue
             path_1 = DIR + release_1['tag'] + '/src'
             path_2 = DIR + release_2['tag'] + '/src'
-            result = subprocess.run('jsinspect -t ' + t_parameter + ' -r json "' + path_1 + '" "' + path_2 + '"', stdout=subprocess.PIPE, shell=True)
-            string = result.stdout.decode('utf-8')
-            parsed = json.loads(string)
-            print("\n\n DONE %d AND %d" % (i_1, i_2))
-            Cij = CalculateCloneSegments(parsed)
-            metric = Cij / (LOCs[i_1] + LOCs[i_2])
-            RESULT[i_1, i_2] = metric
-            print("Calculated metric: %3.6f" % metric)
+            command = 'jsinspect -t ' + t_parameter + ' -r json "' + path_1 + '" "' + path_2 + '"'
+            QUEUE.append( (command, i_1, i_2) )
+    
+    PROCESSES = []
+    while QUEUE:
+        cmd, i_1, i_2 = QUEUE.pop(0)
+        p = mp.Process(target=RunJsInspect, args=(RESULT_QUEUE, cmd, i_1, i_2,))
+        PROCESSES.append(p)
+    
+    RUNNING_PROCESSES = []
 
+    NUM_SEQ = 2
+
+    try:
+        while PROCESSES or RUNNING_PROCESSES:
+            if len(RUNNING_PROCESSES) >= NUM_SEQ or not PROCESSES:
+                p = RUNNING_PROCESSES.pop(0)
+                p.join()
+            else:
+                p = PROCESSES.pop(0)
+                p.start()
+                RUNNING_PROCESSES.append(p)
+    except Exception as e:
+        raise e
+    finally:
+        while not RESULT_QUEUE.empty():
+            (i, j, Cij ) = RESULT_QUEUE.get()
+            metric = Cij / (LOCs[i] + LOCs[j])
+            RESULTS.add(releases[i]['tag'], releases[j]['tag'], metric)
+            print("Calculated metric: %3.6f" % metric)
+        
+        RESULTS.save()
+
+    print("\nDONE AFTER %4.2f SECS" % (time.time() - START_TIME))
+
+
+    # FALLBACK CODE for non-multiprocess
+    # for i_1, release_1 in enumerate(releases):
+    #     for i_2, release_2 in enumerate(releases[:i_1]):
+    #         print("\n\n RUNNING %d AND %d" % (i_1, i_2))
+    #         path_1 = DIR + release_1['tag'] + '/src'
+    #         path_2 = DIR + release_2['tag'] + '/src'
+    #         result = subprocess.run('jsinspect -t ' + t_parameter + ' -r json "' + path_1 + '" "' + path_2 + '"', stdout=subprocess.PIPE, shell=True)
+    #         string = result.stdout.decode('utf-8')
+    #         parsed = json.loads(string)
+    #         print("\n\n DONE %d AND %d" % (i_1, i_2))
+    #         Cij = CalculateCloneSegments(parsed)
+    #         metric = Cij / (LOCs[i_1] + LOCs[i_2])
+    #         RESULT[i_1, i_2] = metric
+    #         print("Calculated metric: %3.6f" % metric)
