f9c0e703a8a1c4dcc69d36814d07ba4b47849c72
Maurice2499 <38662712+Maurice2499@users.noreply.github.com>
2021-03-11 14:51:12 +0100

    change in base snippet

--- JsInspect Analysis/base_snippet.js
+++ JsInspect Analysis/base_snippet.js
@@ -1,21 +1,21 @@
 
-getPrimes = function(N) {
-    let candidate = 1;
-    let arr = [2];
-    while (arr.length < N) {
-        candidate += 2;
-        let candidateIsShownComposite = false;
-        for (var i = 0; i < arr.length; i++) {
-            if (candidate % arr[i] == 0) {
-                candidateIsShownComposite = true;
-                break;
-            }
-        }
-        if (!candidateIsShownComposite) {
-            arr.push(candidate);
-        }
-    }
-    return arr;
+getPrimes = function( N ) {
+	let candidate = 1;
+	let arr = [ 2 ];
+	while ( arr.length < N ) {
+		candidate += 2;
+		let candidateIsShownComposite = false;
+		for ( var i = 0; i < arr.length; i++ ) {
+			if ( candidate % arr[i] == 0 ) {
+				candidateIsShownComposite = true;
+				break;
+			}
+		}
+		if ( !candidateIsShownComposite ) {
+			arr.push( candidate );
+		}
+	}
+	return arr;
 }
 
-console.log(getPrimes(10))
+console.log( getPrimes(10) );
